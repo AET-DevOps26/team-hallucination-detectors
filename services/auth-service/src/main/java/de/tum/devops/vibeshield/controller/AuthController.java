b3f1777b58363b74b741ab428aa12bc3bed69e6d
chouria <azizc922@gmail.com>
2026-05-17 17:52:39 +0200

    setup auth-service

---
+++ services/auth-service/src/main/java/de/tum/devops/vibeshield/controller/AuthController.java
@@ -0,0 +1,15 @@
+package de.tum.devops.vibeshield.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Auth service running";
+    }
+}
